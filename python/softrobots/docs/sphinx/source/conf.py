d0f743200275b71a06518bce20a7379c56fbb604
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-02-02 15:43:15 +0100

    [SoftRobots] Update documentation details.

--- python/softrobots/docs/sphinx/source/conf.py
+++ python/softrobots/docs/sphinx/source/conf.py
@@ -68,9 +68,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'softrobots'
-copyright = u'2018, damien.marchal@univ-lille1.fr'
-author = u'damien.marchal@univ-lille1.fr'
+project = u'SoftRobots'
+copyright = u'Defrost Team'
+author = u'Defrost Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -166,7 +166,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'softrobots.tex', u'softrobots Documentation',
+    (master_doc, 'softrobots.tex', u'SoftRobots Documentation',
      u'damien.marchal@univ-lille1.fr', 'manual'),
 ]
 
@@ -176,7 +176,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'softrobots', u'softrobots Documentation',
+    (master_doc, 'softrobots', u'SoftRobots Documentation',
      [author], 1)
 ]
 
@@ -187,7 +187,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'softrobots', u'softrobots Documentation',
+    (master_doc, 'softrobots', u'SoftRobots Documentation',
      author, 'softrobots', 'One line description of project.',
      'Miscellaneous'),
 ]
