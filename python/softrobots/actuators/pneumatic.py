a2948f871af51f5e9e0914ff1a4c9914e1b872ad
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-02-12 12:51:05 +0100

    [SoftRobots] Refactor python/softrobots/actuators/pneumatic.py
    - first try of the naming scheme for defining the attaching rules
    - unify to use the "with" prefix for optional parameters
    - unify to use the fromSurfaceMesh for the cavity definition.
    - add the "SoftRobots" in the MainHeader creation.
    
    NB: the test scene still does not work.

--- python/softrobots/actuators/pneumatic.py
+++ python/softrobots/actuators/pneumatic.py
@@ -1,5 +1,16 @@
-def PneumaticCavity(node, name="pneumatic",
-    cavityMeshFile, value=0, valueType="volumeGrowth"):
+def getOrCreateTheTemplateNode(attachedAsAChildOf=None, attachedTo=None, withName=None):
+    if attachedTo != None:
+        if withName != None:
+            Sofa.msg_error(attachedTo, "The withName parameter cannot be used when a template is attachedTo to an existing node")
+            return attachedTo
+        if attachedAsAChildOf != None:
+            Sofa.msg_error(attachedTo, "Both attachedTo and attachedAsAChildOf are set is not allowed.")
+            return attachedTo
+        return attachedTo
+    return attachedAsAChildOf.createChild(withName)
+
+def PneumaticCavity(fromSurfaceMesh, attachedAsAChildOf,
+                    withName="PneumaticCavity", withValue=0, withValueType="volumeGrowth"):
     
     """Creates and adds a pneumatic constraint.
     
@@ -8,14 +19,16 @@ def PneumaticCavity(node, name="pneumatic",
     Args:
         cavityMeshFile (string): path to the cavity mesh (the mesh should be a surfacic mesh, ie only triangles or quads).
         
-        value (real): value to apply, default is 0.
+        withName (string): name of the created node.
+
+        withValue (real): value to apply, default is 0.
         
-        valueType (string): type of the parameter value (volumeGrowth or pressure), default is volumeGrowth.
+        withValueType (string): type of the parameter value (volumeGrowth or pressure), default is volumeGrowth.
         
     Structure:
     .. sourcecode:: qml
         Node : {
-                name : "pneumatic"
+                name : "PneumaticCavity"
                 MeshTopology,
                 MechanicalObject,
                 SurfacePressureConstraint,
@@ -24,11 +37,14 @@ def PneumaticCavity(node, name="pneumatic",
         
     """
     
+    node = getOrCreateTheTemplateNode(attachedAsAChildOf=attachedAsAChildOf,
+                                      withName=withName)
+
     #  This create a new node in the scene. This node is appended to the finger's node.
-    pneumatic = node.createChild(name)
+    pneumatic = node.createChild(withName)
     
     # This create a MeshTopology, a componant loading and holding the topology of the cavity.
-    pneumatic.createObject('MeshTopology', name="topology", filename=cavityMeshFile)
+    pneumatic.createObject('MeshTopology', name="topology", filename=fromSurfaceMesh)
 
     # This create a MechanicalObject, a componant holding the degree of freedom of our
     # mechanical modelling. In the case of a cavity actuated with pneumatic, it is a set of positions specifying
@@ -39,16 +55,19 @@ def PneumaticCavity(node, name="pneumatic",
     # the indices are referring to the MechanicalObject's positions.
     # The last indice is where the pullPoint is connected.
     pneumatic.createObject('SurfacePressureConstraint',
-                          value=value,
-                          valueType=valueType)
+                          value=withValue,
+                          valueType=withValueType)
                         
     # This create a BarycentricMapping. A BarycentricMapping is a key element as it will create a bi-directional link
     # between the cavity's DoFs and the parents's ones so that the pressure applied on the cavity wall will be mapped
     # to the volume structure and vice-versa;
-    pneumatic.createObject('BarycentricMapping', name="mapping", mapForces="false", mapMasses="false")
+    pneumatic.createObject('BarycentricMapping', name="Mapping", mapForces="false", mapMasses="false")
     
     return pneumatic
     
 def createScene(node):
+    from stlib.scene import MainHeader
+    MainHeader(node, plugins=["SoftRobots"])
     node.createObject('MechanicalObject')
-    PneumaticCavity(node)
+
+    PneumaticCavity(fromSurfaceMesh="mesh/cube.obj", attachedAsAChildOf=node)
