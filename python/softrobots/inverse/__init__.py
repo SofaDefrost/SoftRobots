6e97be8bf8b2c41991fe8cf851f564263da9f506
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-02-24 00:33:57 +0100

    [SoftRobots] FIX the softrobots.inverse isAvailable that was missing.

--- python/softrobots/inverse/__init__.py
+++ python/softrobots/inverse/__init__.py
@@ -10,11 +10,14 @@ Content:
 
 from Sofa import msg_error, msg_info
 
+isAvailable = False
+
 ### This is searching for the private part of the plugin. In case this part is not installed
 ### then a message is printed.
 try:
     from __softrobotsinverse__ import isAvailable
     msg_info("SoftRobots", "Loading 'softrobots.inverse' python module.")
+    isAvailable = True
 except:
     msg_error("SoftRobots", """Missing SoftRobots.Inverse.
 This scene is using the SoftRobots.Inverse plugin which does not seem available on your system.
