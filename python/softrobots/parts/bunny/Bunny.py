5af7b2a6d3751795eea1d406bcfaec70aa43678d
Stefan Escaida Navarro <stefan.escaida-navarro@inria.fr>
2018-04-06 15:53:01 +0200

    refactoring example for bunnies

--- python/softrobots/parts/bunny/Bunny.py
+++ python/softrobots/parts/bunny/Bunny.py
@@ -22,38 +22,11 @@ def createBunny(Node, Translation=[0,0,0], ControlType='PressureConstraint', Nam
 
     FixedBox(Bunny, doVisualization=True, atPositions=BoxROICoordinates)
        
-    #Bunny = Node.createChild('Bunny')
-    #Bunny.createObject('EulerImplicit', name='odesolver')
-    #Bunny.createObject('SparseLDLSolver', name='preconditioner')
-    #Bunny.createObject('MeshVTKLoader', name='loader', filename=meshpath+'Hollow_Stanford_Bunny.vtu', translation=Translation)
-    #Bunny.createObject('TetrahedronSetTopologyContainer', src='@loader', name='container')
-    #Bunny.createObject('TetrahedronSetTopologyModifier')
-    #Bunny.createObject('TetrahedronSetTopologyAlgorithms', template='Vec3d')
-    #Bunny.createObject('TetrahedronSetGeometryAlgorithms', template='Vec3d')
-    #Bunny.createObject('MechanicalObject', name='tetras', template='Vec3d')
-    #Bunny.createObject('UniformMass', totalmass='0.5')
-    #Bunny.createObject('TetrahedronFEMForceField', template='Vec3d', name='FEM', poissonRatio='0.3',  youngModulus=YoungModulus)
-    #Bunny.createObject('BoxROI', name='boxROI', box=BoxROICoordinates, drawBoxes='true', position="@tetras.rest_position", tetrahedra="@container.tetrahedra")
-    #Bunny.createObject('RestShapeSpringsForceField', points='@boxROI.indices', stiffness='1e12')
-    #Bunny.createObject('LinearSolverConstraintCorrection')
 
     if ControlType == 'PressureConstraint':
         cavity = PneumaticCavity(name='Cavity',attachedAsAChildOf=Bunny,surfaceMeshFileName=meshpath+'Hollow_Bunny_Body_Cavity.obj',valueType='pressureGrowth', initialValue=0.0001)
     elif ControlType=='VolumeConstraint':
         cavity = PneumaticCavity(name='Cavity',attachedAsAChildOf=Bunny,surfaceMeshFileName=meshpath+'Hollow_Bunny_Body_Cavity.obj',valueType='volumeGrowth', initialValue=0.0001)
-        
-    #cavity = Bunny.createChild('cavity')
-    #cavity.createObject('MeshObjLoader', name='loader', filename=meshpath+'Hollow_Bunny_Body_Cavity.obj', translation=Translation)
-    #cavity.createObject('Mesh', src='@loader', name='topo')
-    #cavity.createObject('MechanicalObject', name='cavity')
-    
-    #if(ControlType=='PressureConstraint'):
-        #cavity.createObject('SurfacePressureConstraint', triangles='@topo.triangles', value='40')
-    #elif (ControlType=='VolumeConstraint'):
-        #cavity.createObject('SurfacePressureConstraint', triangles='@topo.triangles', value='40')
-        
-    #cavity.createObject('BarycentricMapping', name='mapping',  mapForces='false', mapMasses='false')
-    
     
     BunnyVisu = Bunny.createChild('visu')
     BunnyVisu.createObject('TriangleSetTopologyContainer', name='container')
