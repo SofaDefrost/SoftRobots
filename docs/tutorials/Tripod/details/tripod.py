94f5c4f2ef2a20b5acacbb590452134fc3c99f1a
eulalie <eulalie.coevoet@inria.fr>
2018-04-10 16:41:29 +0200

    [docs] changes in details/tripod.py for more flexibility and updated tripodInverse.pyscn (not working yet)

--- docs/tutorials/Tripod/details/tripod.py
+++ docs/tutorials/Tripod/details/tripod.py
@@ -1,22 +1,22 @@
 """ Sofa model for the "tripod' robot
 
-    This model is part of the SoftRobot toolkit available at:
-        https://github.com/SofaDefrost/SoftRobots
+	This model is part of the SoftRobot toolkit available at:
+		https://github.com/SofaDefrost/SoftRobots
 
-    List of available parts:
-        - Tripod
-        - ElasticBody
-        - ActuatedArmWithConstraint (add to a s90 ActuatedArm a set of constraints to attach a deformable object )
+	List of available parts:
+		- Tripod
+		- ElasticBody
+		- ActuatedArmWithConstraint (add to a s90 ActuatedArm a set of constraints to attach a deformable object )
 
-    Example of use:
-        def createScene(rootNode):
-            from stlib.scene import MainHeader
-            r = MainHeader(rootNode, plugins=["SoftRobots"])
-            r.getObject("VisualStyle").displayFlags="showForceFields"
+	Example of use:
+		def createScene(rootNode):
+			from stlib.scene import MainHeader
+			r = MainHeader(rootNode, plugins=["SoftRobots"])
+			r.getObject("VisualStyle").displayFlags="showForceFields"
 
-            tripod = Tripod(rootNode)
+			tripod = Tripod(rootNode)
 
-            return rootNode
+			return rootNode
 """
 from stlib.scene import Node
 from stlib.components import OrientedBoxRoi
@@ -27,83 +27,88 @@ from math import sin,cos
 from parts import ActuatedArm, ServoMotor, ElasticBody
 
 def Tripod(parentNode, numMotors=3, radius=4.6, angleShift=180):
-    tripod = Node(parentNode, 'Tripod')
+	tripod = Node(parentNode, 'Tripod')
 
-  
-    em = ElasticBody(tripod, rotation=[90.0,0.0,0.0],
-                     name="ElasticBody")
-    eb = em.getChild("ElasticMaterialObject")
-    dist = radius
-    numstep = numMotors
-    for i in range(0,numstep):
-        name = "ActuatedArm"+str(i)
-        fi = float(i)
-        fnumstep = float(numstep)
-        angle = fi*360/fnumstep
-        angle2 = fi*360/fnumstep+angleShift
-        eulerRotation = [0,angle,0]
-        translation = [dist*sin(to_radians(angle2)), -1.35, dist*cos(to_radians(angle2))]
 
-        c = ActuatedArmWithConstraint(tripod, name=name, position=get(em, "ElasticMaterialObject/MechanicalObject.rest_position").getLinkPath(),
-                                      translation=translation, eulerRotation=eulerRotation)
+	em = ElasticBody(tripod, rotation=[90.0,0.0,0.0],
+					 name="ElasticBody")
+	eb = em.getChild("ElasticMaterialObject")
+	dist = radius
+	numstep = numMotors
+	for i in range(0,numstep):
+		name = "ActuatedArm"+str(i)
+		fi = float(i)
+		fnumstep = float(numstep)
+		angle = fi*360/fnumstep
+		angle2 = fi*360/fnumstep+angleShift
+		eulerRotation = [0,angle,0]
+		translation = [dist*sin(to_radians(angle2)), -1.35, dist*cos(to_radians(angle2))]
 
-        eb.createObject('RestShapeSpringsForceField',
-                    points=get(c, 'Constraint/BoxROI.indices').getLinkPath(),
-                    external_rest_shape=get(c, 'Constraint/MechanicalObject').getLinkPath(), stiffness='1e12')
+		c = ActuatedArmWithConstraint(tripod, name=name, position=get(em, "ElasticMaterialObject/MechanicalObject.rest_position").getLinkPath(),
+									  translation=translation, eulerRotation=eulerRotation)
 
-    return tripod
+	return tripod
 
 def ActuatedArmWithConstraint(parentNode, name="ActuatedArm", position=[], translation=[0,0,0], eulerRotation=[0,0,0]):
 
-    arm = ActuatedArm(parentNode, name=name,
-                      translation=translation,
-                      eulerRotation=eulerRotation)
+	arm = ActuatedArm(parentNode, name=name,
+					  translation=translation,
+					  eulerRotation=eulerRotation)
 
-    constraint = Node(arm, "Constraint")
-    o = OrientedBoxRoi(constraint, position=position,
-                     translation=vadd(translation, [0.0,1.0,0.0]),
-                     eulerRotation=eulerRotation, scale=[2.5,1,0.9])
+	constraint = Node(arm, "Constraint")
+	o = OrientedBoxRoi(constraint, position=position,
+					 translation=vadd(translation, [0.0,1.0,0.0]),
+					 eulerRotation=eulerRotation, scale=[2.5,1,0.9])
 
-    o.drawSize = 5
-    constraint.createObject("TransformEngine", input_position="@BoxROI.pointsInROI",
-                            translation=translation, rotation=eulerRotation, inverse=True )
+	o.drawSize = 5
+	constraint.createObject("TransformEngine", input_position="@BoxROI.pointsInROI",
+							translation=translation, rotation=eulerRotation, inverse=True )
 
-    constraint.createObject("MechanicalObject", template="Vec3d", position="@TransformEngine.output_position",
-                            showObject=True, showObjectScale=10.0)
+	constraint.createObject("MechanicalObject", template="Vec3d", position="@TransformEngine.output_position",
+							showObject=True, showObjectScale=10.0)
 
-    constraint.createObject('RigidMapping', input="@../ServoMotor/ServoWheel/MechanicalObject", output="@./")
+	constraint.createObject('RigidMapping', input="@../ServoMotor/ServoWheel/MechanicalObject", output="@./")
 
-    return arm
+	return arm
 
 def addSimulationPlan(parentNode, modelNode):
-    parentNode.createObject("DefaultAnimationLoop")
-    parentNode.createObject("DefaultVisualManagerLoop")
+	parentNode.createObject("DefaultAnimationLoop")
+	parentNode.createObject("DefaultVisualManagerLoop")
 
-    part1 = Node(parentNode, "MechanicalPart1")
-    part1.createObject("EulerImplicit")
-    part1.createObject("SparseLDLSolver")
-    part1.addChild( get(modelNode, "ElasticBody") )
-    
-    #get(modelNode, "ElasticBody/ElasticMaterialObject").createObject("LinearSolverConstraintCorrection", solverName="SparseLDLSolver")
+	part1 = Node(parentNode, "MechanicalPart1")
+	part1.createObject("EulerImplicit")
+	part1.createObject("SparseLDLSolver")
+	part1.addChild(get(modelNode, "ElasticBody"))
 
-    part2 = Node(parentNode, "MechanicalPart2")
-    part2.createObject("EulerImplicit")
-    part2.createObject("CGLinearSolver")
-    part2.addChild( get(modelNode, "ActuatedArm0") )
-    part2.addChild( get(modelNode, "ActuatedArm1") )
-    part2.addChild( get(modelNode, "ActuatedArm2") )
-    return parentNode
+	#get(modelNode, "ElasticBody/ElasticMaterialObject").createObject("LinearSolverConstraintCorrection", solverName="SparseLDLSolver")
+
+	part2 = Node(parentNode, "MechanicalPart2")
+	part2.createObject("EulerImplicit")
+	part2.createObject("CGLinearSolver")
+
+	for i in range(0,3):
+		a = get(modelNode, "ActuatedArm"+str(i))
+		part2.addChild(a)
+
+		eb = get(modelNode, "ElasticBody")
+		em = eb.getChild("ElasticMaterialObject")
+		em.createObject('RestShapeSpringsForceField',
+    					 points=get(a, 'Constraint/BoxROI.indices').getLinkPath(),
+    					 external_rest_shape=get(a, 'Constraint/MechanicalObject').getLinkPath(),
+    					 stiffness='1e12')
+
+	return parentNode
 
 #Units: cm and kg
 def createScene(rootNode):
-    from stlib.scene import MainHeader
-    r = MainHeader(rootNode, plugins=["SoftRobots"])
-    r.getObject("VisualStyle").displayFlags="showForceFields"
+	from stlib.scene import MainHeader
+	r = MainHeader(rootNode, plugins=["SoftRobots"])
+	r.getObject("VisualStyle").displayFlags="showForceFields"
 
-    m = Node(rootNode, "Model")
-    tripod = Tripod(m)
+	m = Node(rootNode, "Model")
+	tripod = Tripod(m)
 
-    n = Node(rootNode, "SimulationPlan")
-    addSimulationPlan(n, tripod)
+	n = Node(rootNode, "SimulationPlan")
+	addSimulationPlan(n, tripod)
 
-    return rootNode
+	return rootNode
