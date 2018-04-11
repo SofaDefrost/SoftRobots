ddb43d2912fd158b8c4a42f49d83050fd27021ea
Damien Marchal <damien.marchal@univ-lille1.fr>
2018-04-11 23:12:57 +0200

    [SoftRobots] Put everything into a single 'tripod.py' scene
    Tripod is now made of
    	BaseTripod
    	addInversibleConstraint
    	addDirectConstraint
    
    Not sure this will be the final design (or the one we should do in the tutorial)

--- docs/tutorials/Tripod/details/tripod.py
+++ docs/tutorials/Tripod/details/tripod.py
@@ -25,10 +25,18 @@ from stlib.solver import DefaultSolver
 from stlib.numerics import *
 from math import sin,cos
 from parts import ActuatedArm, ServoMotor, ElasticBody
+from softrobots.inverse.effectors import PositionEffector, EffectorGoal
 
-def Tripod(parentNode, numMotors=3, radius=4.6, angleShift=180):
+def Tripod(parentNode, numMotors=3, radius=4.6, angleShift=180, invertible=False):
+    bt = BaseTripod(parentNode, numMotors, radius, angleShift)
+    if invertible:
+        addInvertibleSpringConstraint(bt, numMotors)
+    else:
+        addSpringConstraint(bt, numMotors)
+    return bt
+
+def BaseTripod(parentNode, numMotors=3, radius=4.6, angleShift=180):
 	tripod = Node(parentNode, 'Tripod')
-
 
 	em = ElasticBody(tripod, rotation=[90.0,0.0,0.0],
 					 name="ElasticBody")
@@ -71,44 +79,140 @@ def ActuatedArmWithConstraint(parentNode, name="ActuatedArm", position=[], trans
 
 	return arm
 
-def addSimulationPlan(parentNode, modelNode):
-	parentNode.createObject("DefaultAnimationLoop")
-	parentNode.createObject("DefaultVisualManagerLoop")
+# The components StiffSpringForceField needs a list of springs of the form:
+#                [node_i_model1, node_i_model2, stiffness, damping, restLength]
+# This list allows to set up each spring between model1 and model2
+# The function GenerateSprings generate this list from the following arguments:
+#   indices1 : list of indices of model1
+#   indices2 : list of indices of model2 (size should be the same of indices1)
+#   stiffness : scalar, stiffness of all the springs
+#   damping : scalar, damping of all the springs
+#   restLength : scalar, length at rest of all the springs
+def GenerateSprings(indices1, indices2, stiffness=1e12, damping=5, restLength=0.001):
 
-	part1 = Node(parentNode, "MechanicalPart1")
-	part1.createObject("EulerImplicit")
-	part1.createObject("SparseLDLSolver")
-	part1.addChild(get(modelNode, "ElasticBody"))
+	spring = []
+	size1 = len(indices1)
+	size2 = len(indices2)
 
-	#get(modelNode, "ElasticBody/ElasticMaterialObject").createObject("LinearSolverConstraintCorrection", solverName="SparseLDLSolver")
+	if size1 != size2:
+		return spring
 
-	part2 = Node(parentNode, "MechanicalPart2")
-	part2.createObject("EulerImplicit")
-	part2.createObject("CGLinearSolver")
+	spring = [[0.,0.,0.,0.,0.]]*size1
+	for i in range(0,size1):
+		spring[i] = [indices1[i],indices2[i],stiffness,damping,restLength]
 
-	for i in range(0,3):
-		a = get(modelNode, "ActuatedArm"+str(i))
-		part2.addChild(a)
+	return spring
 
-		eb = get(modelNode, "ElasticBody")
-		em = eb.getChild("ElasticMaterialObject")
-		em.createObject('RestShapeSpringsForceField',
-    					 points=get(a, 'Constraint/BoxROI.indices').getLinkPath(),
-    					 external_rest_shape=get(a, 'Constraint/MechanicalObject').getLinkPath(),
-    					 stiffness='1e12')
+def addInvertibleSpringConstraint(modelNode, numMotors=3):
+    ## Initialisation needed for the generation of springs
+    elasticBody = get(modelNode, "ElasticBody/ElasticMaterialObject")
+    get(elasticBody, 'MeshLoader').init()
+    get(elasticBody, 'TetrahedronSetTopologyContainer').init()
+    get(elasticBody, 'MechanicalObject').init()
 
-	return parentNode
+    for i in range(0, numMotors):
+        actuatedArm = get(modelNode, "ActuatedArm"+str(i))
+        
+        wheel = get(actuatedArm, 'ServoMotor/ServoWheel')
+        wheel.createObject('SlidingActuator', template="Rigid", indices="0", direction="0 0 0 1 0 0")
+
+        ## Initialisation needed for the generation of springs
+        boxROI = get(actuatedArm, 'Constraint/BoxROI')
+        boxROI.init()
+        spring = GenerateSprings(range(0,boxROI.nbIndices), get(actuatedArm, 'Constraint/BoxROI.indices').value)
+
+        elasticBody.createObject('StiffSpringForceField',
+                                                        name='StiffSpringForceField'+str(i),
+                                                        spring=spring,
+                                                        object1=get(actuatedArm, 'Constraint/MechanicalObject').getLinkPath(),
+                                                        object2="@./")
+
+        elasticBody.createObject('MappedMatrixForceFieldAndMass',
+                                                        template="Vec3d,Rigid3d",
+                                                        name='MappedMatrixForceFieldAndMass'+str(i),
+                                                        object1=get(actuatedArm,'Constraint/MechanicalObject').getLinkPath(),
+                                                        object2=get(actuatedArm,'MechanicalObject').getLinkPath(),
+                                                        mappedForceField="@./TetrahedronFEMForceField",
+                                                        mappedMass="@./UniformMass")
+
+
+def addSpringConstraint(modelNode, numMotors):
+    for i in range(0, numMotors):
+            a = get(modelNode, "ActuatedArm"+str(i))
+
+            eb = get(modelNode, "ElasticBody")
+            em = eb.getChild("ElasticMaterialObject")
+            em.createObject('RestShapeSpringsForceField',
+                                     points=get(a, 'Constraint/BoxROI.indices').getLinkPath(),
+                                     external_rest_shape=get(a, 'Constraint/MechanicalObject').getLinkPath(),
+                                     stiffness='1e12')
+
+
+def addDirectSimulationPlan(parentNode, modelNode):
+    parentNode.createObject("DefaultAnimationLoop")
+    parentNode.createObject("DefaultVisualManagerLoop")
+
+    part1 = Node(parentNode, "MechanicalPart1")
+    part1.createObject("EulerImplicit")
+    part1.createObject("SparseLDLSolver")
+    part1.addChild(get(modelNode, "ElasticBody"))
+
+    part2 = Node(parentNode, "MechanicalPart2")
+    part2.createObject("EulerImplicit")
+    part2.createObject("CGLinearSolver")
+    for i in range(0, 3):
+        part2.addChild(get(modelNode, "ActuatedArm"+str(i)))
+
+    return parentNode
+
+def addInverseSimulationPlan(parentNode, modelNode):
+    parentNode.createObject("DefaultAnimationLoop")
+    parentNode.createObject("DefaultVisualManagerLoop")
+    parentNode.createObject("QPInverseProblemSolver")
+
+    goal = EffectorGoal(parentNode, position=[[0,4,0]], visuScale=0.2)
+
+    part1 = Node(parentNode, "MechanicalPart1")
+    part1.createObject("EulerImplicit")
+    part1.createObject("SparseLDLSolver")
+    part1.addChild( get(modelNode, "ElasticBody") )
+
+    PositionEffector(get(modelNode, "ElasticBody/ElasticMaterialObject"),
+                         position=[[0,0,0]],
+                         effectorGoal=get(goal, 'MechanicalObject.position').getLinkPath())
+    
+    part2 = Node(parentNode, "MechanicalPart2")
+    part2.createObject("EulerImplicit")
+    part2.createObject("CGLinearSolver")
+    #part2.addChild( get(modelNode, "ActuatedArm1") )
+    #part2.addChild( get(modelNode, "ActuatedArm2") )
+    #part2.addChild( get(modelNode, "ActuatedArm3") )
+
+
+    return parentNode
+
 
 #Units: cm and kg
 def createScene(rootNode):
-	from stlib.scene import MainHeader
-	r = MainHeader(rootNode, plugins=["SoftRobots"])
-	r.getObject("VisualStyle").displayFlags="showForceFields"
+    import Sofa
+    from stlib.scene import MainHeader
+    r = MainHeader(rootNode, plugins=["SoftRobots"])
+    r.getObject("VisualStyle").displayFlags="showForceFields"
+        
+    if len(sys.argv) == 2 and sys.argv[1] == "inverse":
+        Sofa.msg_info("CreateScene(Tripod)", "Loading the scene with the inverse model")
+        m = Node(rootNode, "Model")
+        tripod = Tripod(m, invertible=True)
 
-	m = Node(rootNode, "Model")
-	tripod = Tripod(m)
+        n = Node(rootNode, "SimulationPlan")
+        addInverseSimulationPlan(n, tripod)
 
-	n = Node(rootNode, "SimulationPlan")
-	addSimulationPlan(n, tripod)
+    else:
+        Sofa.msg_info("CreateScene(Tripod)", "Loading the scene with the direct model")
+        m = Node(rootNode, "Model")
+        tripod = Tripod(m)
+
+        n = Node(rootNode, "SimulationPlan")
+        addDirectSimulationPlan(n, tripod)
 
 	return rootNode
