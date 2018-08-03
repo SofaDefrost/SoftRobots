5ad79585120e5d45f7f5079ce8b7bb946c53a4ce
louise-scherrer <louisescherrer@wanadoo.fr>
2018-08-03 18:08:15 +0200

    Attempting to implement a scene with the maze
    Based on the step8 of the tripod-tuto

---
+++ docs/tutorials/Tripod/Rigidification/maze.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+""" Model for a maze coaster
+"""
+import Sofa
+from splib.numerics import RigidDof
+from splib.objectmodel import *
+
+from stlib.visuals import VisualModel
+from stlib.solver import DefaultSolver
+from stlib.scene import Scene, Node, get
+
+@SofaPrefab
+class Maze(SofaObject):
+    """A circular maze coaster
+
+    This prefab is implementing a maze coaster.
+    """
+    def __init__(self, parent,
+                 translation=[0.0,0.0,0.0], rotation=[0.0,0.0,0.0], scale=[1.0,1.0,1.0], doAddVisualModel=True):
+        self.node = Node(parent, "Maze")
+       
+        self.dofs = self.node.createObject("MechanicalObject", size=1,
+                                          name="dofs",
+                                          translation=translation, rotation=rotation, scale=scale,
+                                          template='Rigid', showObject=True, showObjectScale=15)
+        totalMass = 1.0
+        volume = 1.0
+        inertiaMatrix = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+
+        #self.mass = self.node.createObject('UniformMass', name="mass", mass=[totalMass, volume, inertiaMatrix[:]])
+
+
+        if doAddVisualModel:
+            self.addVisualModel()
+
+    def addVisualModel(self):
+        self.visualmodel = VisualModel(self.node, 'data/Maze_Coaster.stl')
+        self.visualmodel.node.createObject('RigidMapping', name = "mapping")
+
+#class MazeUpdater(Sofa.PythonScriptController):
+    ## voir scene controller
+
+
+def createScene(rootNode):
+    from splib.animation import animate
+    from splib.animation.easing import LinearRamp
+    from splib.scenegraph import get
+
+    from stlib.scene import MainHeader
+    Scene(rootNode)
+    s=DefaultSolver(rootNode)
+
+    maze = Maze(rootNode, translation=[2,0,0])
