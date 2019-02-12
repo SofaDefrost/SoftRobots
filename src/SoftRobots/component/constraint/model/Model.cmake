1eba0c7805263eaf3e98c9c23bdbb328d06f2c5e
MARQUES Bruno <bruno.josue.marques@inria.fr>
2019-02-12 15:23:52 +0100

    [SoftRobots] Refactor plugin sources to comply with SOFA's plugin repository structure

---
+++ src/SoftRobots/component/constraint/model/Model.cmake
@@ -0,0 +1,19 @@
+
+
+list(APPEND HEADER_FILES
+
+    ${SOFTROBOTS_SOURCE_DIR}/component/constraint/model/CableModel.h
+    ${SOFTROBOTS_SOURCE_DIR}/component/constraint/model/CableModel.inl
+    ${SOFTROBOTS_SOURCE_DIR}/component/constraint/model/SurfacePressureModel.h
+    ${SOFTROBOTS_SOURCE_DIR}/component/constraint/model/SurfacePressureModel.inl
+
+    )
+
+list(APPEND SOURCE_FILES
+
+    ${SOFTROBOTS_SOURCE_DIR}/component/constraint/model/CableModel.cpp
+    ${SOFTROBOTS_SOURCE_DIR}/component/constraint/model/SurfacePressureModel.cpp
+    
+    )
+
+
