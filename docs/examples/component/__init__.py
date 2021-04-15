2585a6a820ca1b38f545bd7fb17690ecc4a67a70
EulalieCoevoet <eulalie.coevoet@gmail.com>
2021-04-15 09:36:44 -0400

    Update __init__.py

--- docs/examples/component/__init__.py
+++ docs/examples/component/__init__.py
@@ -3,7 +3,7 @@
 The SoftRobots plugin for SOFA: Components Library Documentation
 ================================================================
 
-The SoftRobots plugin for SOFA contains specific SOFA components dedicated to soft robotics. You can find for example, models for cable and pneumatic actuations, tools to define trajectories for the robot's end effector, or tools to communicate with microcontroller boards. 
+The SoftRobots plugin contains SOFA components dedicated to soft robotics. You can find for example, models for cable and pneumatic actuations, tools to define trajectories for the robot's end effector, or tools to communicate with microcontroller boards. 
 
 All these components are discribed in this documentation.
 
@@ -29,4 +29,4 @@ Indices and tables
 * :ref:`search`
 """
 
-__all__=["constraint","controller", "engine"]
+__all__=["constraint","controller","engine"]
