ada39f05af25c77aa9d59909506fde552761ab68
eulaliecoevoet <eulalie.coevoet@gmail.com>
2021-09-23 15:37:05 +0200

    [constraint] fix compile (mask removed)

--- src/SoftRobots/component/behavior/SoftRobotsBaseConstraint.h
+++ src/SoftRobots/component/behavior/SoftRobotsBaseConstraint.h
@@ -131,13 +131,6 @@ public:
 
     virtual void storeResults(type::vector<double> &delta);
 
-    /// Useful when the Constraint is applied only on a subset of dofs.
-    /// It is automatically called by buildConstraintMatrix
-    ///
-    /// That way, we can optimize the time spent to transfer quantities through the mechanical mappings.
-    /// Every Dofs are inserted by default. The Constraint using only a subset of dofs should only insert these dofs in the mask.
-    void updateForceMask() override {}
-
 
 protected:
 
