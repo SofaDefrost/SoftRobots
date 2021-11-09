7ed35dd61c342b16487629cdc2c0a9a958e1e9d7
eulaliecoevoet <eulalie.coevoet@gmail.com>
2021-11-09 15:30:45 -0500

    [SoftRobots] fixes CableGripper tutorial for sofapython3

--- docs/tutorials/CableGripper/details/sofapython3/grippercontroller.py
+++ docs/tutorials/CableGripper/details/sofapython3/grippercontroller.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import Sofa.Core
+import Sofa.constants.Key as Key
+
 
 def getTranslated(points, vec):
     r=[]
@@ -7,31 +9,32 @@ def getTranslated(points, vec):
         r.append( [v[0]+vec[0], v[1]+vec[1], v[2]+vec[2]] )
     return r
 
+
 class GripperController(Sofa.Core.Controller):
-    def __init__(self, node, fingers):
-        self.fingers = fingers
-        self.name = "FingerController"
 
-    def onKeyPressed(self,c):
-        dir = None
-        # UP key :
-        if ord(c)==19:
-            dir = [0.0,1.0,0.0]
-        # DOWN key : rear
-        elif ord(c)==21:
-            dir = [0.0,-1.0,0.0]
-        # LEFT key : left
-        elif ord(c)==18:
-            dir = [1.0,0.0,0.0]
-        elif ord(c)==20:
-            dir = [-1.0,0.0,0.0]
+    def __init__(self, *args, **kwargs):
+        Sofa.Core.Controller.__init__(self,args,kwargs)
+        self.fingers = args[0]
+        self.name = "GripperController"
 
-        if dir != None:
+    def onKeypressedEvent(self,e):
+        direction = None
+
+        if e["key"]==Key.uparrow:
+            direction = [0.0, 1.0, 0.0]
+        elif e["key"]==Key.downarrow:
+            direction = [0.0, -1.0, 0.0]
+        elif e["key"]==Key.leftarrow:
+            direction = [1.0, 0.0, 0.0]
+        elif e["key"]==Key.rightarrow:
+            direction = [-1.0, 0.0, 0.0]
+
+        if direction != None:
             for finger in self.fingers:
                 m = finger.getChild("ElasticMaterialObject")
                 mecaobject = m.getObject("dofs")
-                mecaobject.findData('rest_position').value = getTranslated( mecaobject.rest_position,  dir )
+                mecaobject.findData('rest_position').value = getTranslated(mecaobject.rest_position.value, direction)
 
                 cable = m.getChild("PullingCable").getObject("CableConstraint")
-                p = cable.pullPoint[0]
-                cable.findData("pullPoint").value = [p[0]+dir[0], p[1]+dir[1], p[2]+dir[2]]
+                p = cable.pullPoint.value
+                cable.findData("pullPoint").value = [p[0] + direction[0], p[1] + direction[1], p[2] + direction[2]]
