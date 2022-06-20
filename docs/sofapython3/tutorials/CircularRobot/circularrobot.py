487f1eb79c7de900b5170eea0116adef6e7c5f36
EulalieCoevoet <eulalie.coevoet@gmail.com>
2022-06-20 22:47:48 +0200

    [docs] CircularRobot: cleaning

--- docs/sofapython3/tutorials/CircularRobot/circularrobot.py
+++ docs/sofapython3/tutorials/CircularRobot/circularrobot.py
@@ -28,15 +28,7 @@ def Floor(parentNode, color=[0.5, 0.5, 0.5, 1.], rotation=[90, 0, 180], translat
     return floor
 
 
-def Wall(parentNode, color=[0.5, 0.5, 0.5, 1.], rotation=[0, 0, 0], translation=[-200, -271, 100]):
-    wall = parentNode.addChild('Wall')
-    wall.addObject('MeshOBJLoader', name='loader', filename='mesh/square1.obj', scale=250, rotation=rotation,
-                   translation=translation)
-    wall.addObject('OglModel', src='@loader', color=color)
-    return wall
-
-
-class CircularRobot():
+class CircularRobot:
     """ This prefab is implementing a soft circular robot actuated with four cables.
 
         The prefab is composed of:
@@ -57,10 +49,16 @@ class CircularRobot():
     """
 
     def __init__(self, parentNode, youngModulus=500, poissonRatio=0.3, totalMass=0.042, inverseMode=True,
-                 effectorTarget=None):
+                 effectorTarget=None, withSolver=True):
         self.inverseMode = inverseMode
 
         self.node = parentNode.addChild('CircularRobot')
+
+        if withSolver:
+            self.node.addObject('EulerImplicitSolver', rayleighMass=0.015, rayleighStiffness=0.015)
+            self.node.addObject('SparseLDLSolver')
+            self.node.addObject("GenericConstraintCorrection", solverName="SparseLDLSolver")
+
         self.node.addObject('MeshVTKLoader', name="loader", filename=path + "wheel.vtk")
         self.node.addObject('TetrahedronSetTopologyContainer', position='@loader.position',
                             tetrahedra="@loader.tetrahedra")
@@ -76,18 +74,20 @@ class CircularRobot():
                                 effectorGoal=effectorTarget,
                                 axis=[1, 1, 1])
         self.__addCables()
+        self.__addVisualModel()
 
     def __addCables(self):
-        cables = ["16 0 5", "-16 0 5", "0 16 5", "0 -16 5", "11 11 5", "-11 -11 5", "11 -11 5", "-11 11 5"]
+        positions = [[16, 0, 5], [-16, 0, 5], [0, 16, 5], [0, -16, 5],
+                     [11, 11, 5], [-11, -11, 5], [11, -11, 5], [-11, 11, 5]]
         for i in range(0, 4):
-            cable = self.node.addChild('cable' + str(i + 1))
+            cable = self.node.addChild('Cable' + str(i + 1))
             cable.addObject('VisualStyle', displayFlags="showInteractionForceFields")
-            cable.addObject('MechanicalObject', position=cables[i * 2] + " " + cables[i * 2 + 1])
+            cable.addObject('MechanicalObject', position=[positions[i * 2], positions[i * 2 + 1]])
             cable.addObject('CableActuator' if self.inverseMode else 'CableConstraint', name="cable", indices=[0, 1],
-                            hasPullPoint=0, minForce=0, maxPositiveDisp=12, maxDispVariation=1.5)
+                            hasPullPoint=False, minForce=0, maxPositiveDisp=12, maxDispVariation=1.5)
             cable.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
 
-    def addVisualModel(self, color=[1., 0., 0., 1.]):
+    def __addVisualModel(self, color=[1., 0., 0., 1.]):
         modelVisu = self.node.addChild('VisualModel')
         modelVisu.addObject('MeshSTLLoader', filename=path + 'wheel_visu.stl')
         modelVisu.addObject('OglModel', color=color)
@@ -105,32 +105,43 @@ class CircularRobot():
 
 
 def createScene(rootNode):
-
     INVERSE = False  # Option to use the inverse solvers from the plugin SoftRobots.Inverse
 
     rootNode.addObject('RequiredPlugin', pluginName=['SoftRobots', 'SofaPython3',
-                        "Sofa.Component.AnimationLoop",  # Needed to use components FreeMotionAnimationLoop
-                        "Sofa.Component.Collision.Detection.Algorithm",
-                        # Needed to use components BVHNarrowPhase, BruteForceBroadPhase, DefaultPipeline
-                        "Sofa.Component.Collision.Detection.Intersection",  # Needed to use components LocalMinDistance
-                        "Sofa.Component.Collision.Geometry",
-                        # Needed to use components LineCollisionModel, PointCollisionModel, TriangleCollisionModel
-                        "Sofa.Component.Collision.Response.Contact",  # Needed to use components DefaultContactManager
-                        "Sofa.Component.Constraint.Lagrangian.Correction",
-                        # Needed to use components GenericConstraintCorrection, UncoupledConstraintCorrection
-                        "Sofa.Component.Constraint.Lagrangian.Solver",  # Needed to use components GenericConstraintSolver
-                        "Sofa.Component.IO.Mesh",  # Needed to use components MeshOBJLoader, MeshSTLLoader, MeshVTKLoader
-                        "Sofa.Component.LinearSolver.Direct",  # Needed to use components SparseLDLSolver
-                        "Sofa.Component.LinearSolver.Iterative",  # Needed to use components CGLinearSolver
-                        "Sofa.Component.Mass",  # Needed to use components UniformMass
-                        "Sofa.Component.ODESolver.Backward",  # Needed to use components EulerImplicitSolver
-                        "Sofa.Component.SolidMechanics.FEM.Elastic",  # Needed to use components TetrahedronFEMForceField
-                        "Sofa.Component.Topology.Container.Constant",  # Needed to use components MeshTopology
-                        "Sofa.Component.Topology.Container.Dynamic",
-                        # Needed to use components TetrahedronSetTopologyContainer, TetrahedronSetTopologyModifier
-                        "Sofa.Component.Visual",  # Needed to use components VisualStyle
-                        "Sofa.GL.Component.Rendering3D",  # Needed to use components OglModel
-                        ])
+                                                     "Sofa.Component.AnimationLoop",
+                                                     # Needed to use components FreeMotionAnimationLoop
+                                                     "Sofa.Component.Collision.Detection.Algorithm",
+                                                     # Needed to use components BVHNarrowPhase, BruteForceBroadPhase, DefaultPipeline
+                                                     "Sofa.Component.Collision.Detection.Intersection",
+                                                     # Needed to use components LocalMinDistance
+                                                     "Sofa.Component.Collision.Geometry",
+                                                     # Needed to use components LineCollisionModel, PointCollisionModel, TriangleCollisionModel
+                                                     "Sofa.Component.Collision.Response.Contact",
+                                                     # Needed to use components DefaultContactManager
+                                                     "Sofa.Component.Constraint.Lagrangian.Correction",
+                                                     # Needed to use components GenericConstraintCorrection, UncoupledConstraintCorrection
+                                                     "Sofa.Component.Constraint.Lagrangian.Solver",
+                                                     # Needed to use components GenericConstraintSolver
+                                                     "Sofa.Component.IO.Mesh",
+                                                     # Needed to use components MeshOBJLoader, MeshSTLLoader, MeshVTKLoader
+                                                     "Sofa.Component.LinearSolver.Direct",
+                                                     # Needed to use components SparseLDLSolver
+                                                     "Sofa.Component.LinearSolver.Iterative",
+                                                     # Needed to use components CGLinearSolver
+                                                     "Sofa.Component.Mass",  # Needed to use components UniformMass
+                                                     "Sofa.Component.ODESolver.Backward",
+                                                     # Needed to use components EulerImplicitSolver
+                                                     "Sofa.Component.SolidMechanics.FEM.Elastic",
+                                                     # Needed to use components TetrahedronFEMForceField
+                                                     "Sofa.Component.Topology.Container.Constant",
+                                                     # Needed to use components MeshTopology
+                                                     "Sofa.Component.Topology.Container.Dynamic",
+                                                     # Needed to use components TetrahedronSetTopologyContainer, TetrahedronSetTopologyModifier
+                                                     "Sofa.Component.Visual",  # Needed to use VisualStyle
+                                                     "Sofa.GL.Component.Rendering3D",
+                                                     # Needed to use OglModel
+                                                     "Sofa.GUI.Component",  # Needed to use AttachBodyButtonSetting
+                                                     ])
 
     rootNode.addObject('VisualStyle',
                        displayFlags='hideWireframe showVisualModels showBehaviorModels hideCollisionModels '
@@ -141,6 +152,8 @@ def createScene(rootNode):
     # Add solver for inverse resolution
     rootNode.addObject('FreeMotionAnimationLoop')
     rootNode.addObject('DefaultVisualManagerLoop')
+    rootNode.addObject('AttachBodyButtonSetting', stiffness=10)
+
     if INVERSE:
         rootNode.addObject('RequiredPlugin', name='SoftRobots.Inverse')
         rootNode.addObject('QPInverseProblemSolver', epsilon=2e-0, maxIterations=2500, tolerance=1e-7,
@@ -155,19 +168,14 @@ def createScene(rootNode):
     rootNode.addObject('DefaultContactManager', response="FrictionContactConstraint", responseParams="mu=0.8")
     rootNode.addObject('LocalMinDistance', alarmDistance=5, contactDistance=1, angleCone=0.0)
 
-    # Add linear solver
-    simulation = rootNode.addChild("Simulation")
-    simulation.addObject('EulerImplicitSolver', rayleighMass=0.015, rayleighStiffness=0.015)
-    simulation.addObject('SparseLDLSolver')
-    simulation.addObject("GenericConstraintCorrection", solverName="SparseLDLSolver")
-
     Floor(rootNode)
+
     if INVERSE:
         target = Target(rootNode)
 
-    circularrobot = CircularRobot(simulation, effectorTarget=target.dofs.getData("position").getLinkPath() if INVERSE else '',
+    circularrobot = CircularRobot(rootNode,
+                                  effectorTarget=target.dofs.getData("position").getLinkPath() if INVERSE else '',
                                   inverseMode=INVERSE)
-    circularrobot.addVisualModel()
     circularrobot.addCollisionModel()
 
     return rootNode
