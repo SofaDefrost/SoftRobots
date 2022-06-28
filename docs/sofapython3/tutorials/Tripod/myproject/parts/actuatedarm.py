7c5491b9a4b5e00453335a3736ec649957ca8783
EulalieCoevoet <eulalie.coevoet@gmail.com>
2022-06-28 10:56:58 -0400

    [docs] Tripod: update myproject for steps 8 and 8-maze (#166)

---
+++ docs/sofapython3/tutorials/Tripod/myproject/parts/actuatedarm.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+""" ActuatedArm for the tripod robot.
+
+    This model is part of the SoftRobot toolkit available at:
+        https://github.com/SofaDefrost/SoftRobots
+
+    Available prefab:
+        - ActuatedArm
+        - ServoArm
+"""
+import Sofa
+from stlib3.visuals import VisualModel
+
+from s90servo import ServoMotor
+
+
+class ServoArm(Sofa.Prefab):
+    """ServoArm is a reusable sofa model of a servo arm for the S90 servo motor
+
+       Parameters:
+            parent:        node where the ServoArm will be attached
+            mappingInput:  the rigid mechanical object that will control the orientation of the servo arm
+            indexInput: (int) index of the rigid the ServoArm should be mapped to
+    """
+
+    prefabData = [
+        {'name': 'name', 'type': 'string', 'help': 'Node name', 'default': 'ServoArm'},
+        {'name': 'mappingInputLink', 'type': 'string',
+         'help': 'the rigid mechanical object that will control the orientation of the servo arm', 'default': ''},
+        {'name': 'indexInput', 'type': 'int', 'help': 'index of the rigid the ServoArm should be mapped to',
+         'default': 1}]
+
+    def __init__(self, *args, **kwargs):
+        Sofa.Prefab.__init__(self, *args, **kwargs)
+
+    def init(self):
+        self.addObject('MechanicalObject',
+                       name='dofs',
+                       size=1,
+                       template='Rigid3',
+                       showObject=True,
+                       showObjectScale=5,
+                       translation2=[0, 25, 0])
+
+    def setRigidMapping(self, path):
+        self.addObject('RigidRigidMapping', name='mapping', input=path, index=self.indexInput.value)
+
+        visual = self.addChild(VisualModel(visualMeshPath='data/mesh/SG90_servoarm.stl', translation=[0., -25., 0.],
+                                           color=[1., 1., 1., 0.75]))
+        visual.OglModel.writeZTransparent = True
+        visual.addObject('RigidMapping', name='mapping')
+
+
+class ActuatedArm(Sofa.Prefab):
+    """ActuatedArm is a reusable sofa model of a S90 servo motor and the tripod actuation arm.
+           Parameters:
+             - translation the position in space of the structure
+             - eulerRotation the orientation of the structure
+
+           Structure:
+           Node : {
+                name : 'ActuatedArm'
+                MechanicalObject     // Rigid position of the motor
+                ServoMotor           // The s90 servo motor with its actuated wheel
+                ServoArm             // The actuation arm connected to ServoMotor.ServoWheel
+            }
+    """
+    prefabData = [
+        {'name': 'name', 'type': 'string', 'help': 'Node name', 'default': 'ActuatedArm'},
+        {'name': 'rotation', 'type': 'Vec3d', 'help': 'Rotation', 'default': [0.0, 0.0, 0.0]},
+        {'name': 'translation', 'type': 'Vec3d', 'help': 'Translation', 'default': [0.0, 0.0, 0.0]},
+        {'name': 'scale', 'type': 'Vec3d', 'help': 'Scale 3d', 'default': [1.0, 1.0, 1.0]}]
+
+    def __init__(self, *args, **kwargs):
+        Sofa.Prefab.__init__(self, *args, **kwargs)
+
+    def init(self):
+        self.servomotor = self.addChild(
+            ServoMotor(name="ServoMotor", translation=self.translation.value, rotation=self.rotation.value))
+        self.servoarm = self.servomotor.Articulation.ServoWheel.addChild(ServoArm(name="ServoArm"))
+        self.servoarm.setRigidMapping(self.ServoMotor.Articulation.ServoWheel.dofs.getLinkPath())
+
+        # add a public attribute and connect it to the private one.
+        self.addData(name='angleIn', group='ArmProperties', help='angle of rotation (in radians) of the arm',
+                     type='float', value=0)
+        self.ServoMotor.getData('angleIn').setParent(self.getData('angleIn'))
+
+        # add a public attribute and connect it to the internal one.
+        self.addData(name='angleOut', group='ArmProperties', help='angle of rotation (in radians) of the arm',
+                     type='float', value=0)
+        self.getData('angleOut').setParent(self.ServoMotor.getData('angleOut'))
+
+
+def createScene(rootNode):
+    from splib3.animation import animate
+    from stlib3.scene import Scene
+    import math
+
+    scene = Scene(rootNode, plugins=['SofaConstraint', 'SofaGeneralRigid', 'SofaRigid'], iterative=False)
+    scene.addMainHeader()
+    scene.addObject('DefaultVisualManagerLoop')
+    scene.addObject('FreeMotionAnimationLoop')
+    scene.addObject('GenericConstraintSolver', maxIterations=50, tolerance=1e-5)
+    scene.Simulation.addObject('GenericConstraintCorrection')
+    scene.VisualStyle.displayFlags = 'showBehavior'
+    scene.dt = 0.01
+    scene.gravity = [0., -9810., 0.]
+
+    arm = scene.Simulation.addChild(ActuatedArm(name='ActuatedArm', translation=[0.0, 0.0, 0.0]))
+
+    def myanimate(target, factor):
+        target.angleIn.value = math.cos(factor * 2 * math.pi)
+
+    animate(myanimate, {'target': arm}, duration=10., mode='loop')
---
+++ docs/sofapython3/tutorials/Tripod/myproject/parts/tripodcontroller.py
@@ -0,0 +1,273 @@
+import Sofa
+from splib3.numerics import RigidDof, Quat
+from splib3.animation import animate
+from splib3.constants import Key
+from stlib3.scene import Scene
+from tripod import Tripod
+import math
+
+def dumpPosition(fields, filename):
+    import json
+    data = {}
+    for field in fields:
+        v = field.value
+        if isinstance(v, list) and isinstance(v[0], list):
+            v = v[0]
+        data[field.getLinkPath()] = v
+    with open(filename, "w+t") as file:
+        json.dump(data, file)
+
+
+def setupanimation(actuators, step, angularstep, factor):
+    """This function is called repeatidely in an animation.
+       It moves the actuators by translating & rotating them according to the factor
+       value.
+    """
+    for actuator in actuators:
+        rigid = RigidDof(actuator.ServoMotor.ServoBody.dofs)
+        rigid.setPosition(rigid.rest_position + rigid.forward * step * factor)
+        actuator.angleIn = angularstep * factor
+
+
+def saveTripodPosition(actuators, step, angularstep, factor):
+        t = []
+        for actuator in actuators:
+                t.append(actuator.ServoMotor.ServoBody.dofs.getData("position"))
+                t.append(actuator.ServoMotor.getData("angleIn"))
+
+        dumpPosition(t, "tripodRestPosition.json")
+
+
+class TripodController(Sofa.Core.Controller):
+    """This controller has two roles:
+       - if the user presses up/left/right/down/plus/minus, the servomotor angle
+         is changed.
+       - if thr user presses A, an animation is started to move the servomotor to the initial position
+         of the real robot.
+    """
+    def __init__(self, *args, **kwargs):
+        # These are needed (and the normal way to override from a python class)
+        Sofa.Core.Controller.__init__(self, *args, **kwargs)
+
+        self.stepsize = 0.1
+        self.actuators = kwargs["actuators"]
+
+    def onKeypressedEvent(self, event):
+        key = event['key']
+        self.initTripod(key)
+        self.animateTripod(key)
+
+    def initTripod(self, key):
+        if key == Key.A:
+            animate(setupanimation,
+                    {"actuators": self.actuators, "step": 35.0, "angularstep": -1.4965},
+                    duration=0.2)
+
+    def animateTripod(self, key):
+        if key == Key.uparrow:
+            self.actuators[0].ServoMotor.angleIn = self.actuators[0].ServoMotor.angleOut.value + self.stepsize
+        elif key == Key.downarrow:
+            self.actuators[0].ServoMotor.angleIn = self.actuators[0].ServoMotor.angleOut.value - self.stepsize
+
+        if key == Key.leftarrow:
+            self.actuators[1].ServoMotor.angleIn = self.actuators[1].ServoMotor.angleOut.value + self.stepsize
+        elif key == Key.rightarrow:
+            self.actuators[1].ServoMotor.angleIn = self.actuators[1].ServoMotor.angleOut.value - self.stepsize
+
+        if key == Key.plus:
+            self.actuators[2].ServoMotor.angleIn = self.actuators[2].ServoMotor.angleOut.value + self.stepsize
+        elif key == Key.minus:
+            self.actuators[2].ServoMotor.angleIn = self.actuators[2].ServoMotor.angleOut.value - self.stepsize
+
+
+class TripodControllerWithCom(TripodController):
+    """This controller has three roles:
+       - if the user presses up/left/right/down/plus/minus, the servomotor angle
+         is changed.
+       - if thr user presses A, an animation is started to move the servomotor to the initial position
+         of the real robot.
+       - if thr user presses B start the communication with controller card, send
+         servomotor commands
+    """
+
+    def __init__(self, node, actuators, serialportctrl):
+        TripodController.__init__(self, node, actuators=actuators)
+        self.serialportctrl = serialportctrl
+
+    def initTripod(self, key):
+        if key == Key.A and self.serialportctrl.state == "init":
+            self.serialportctrl.state = "no-comm"
+            animate(setupanimation, {"actuators": self.actuators, "step": 35.0, "angularstep": -1.4965}, duration=0.2)
+
+        # Inclusion of the keystroke to start data sending = establishing communication ('comm')
+        if key == Key.B and self.serialportctrl.state == "no-comm":
+            self.serialportctrl.state = "comm"
+
+
+# Description of how the communication is handled
+# CHANGE HERE the serialport that correspond to your computer
+# def SerialPortBridgeGeneric(rootNode, serialport='/dev/cu.usbserial-1420'):
+# def SerialPortBridgeGeneric(rootNode, serialport='COM3'):
+def SerialPortBridgeGeneric(rootNode, serialport="/dev/ttyUSB0"):
+    return rootNode.addObject("SerialPortBridgeGeneric", port=serialport, baudRate=115200, size=3, listening=True, header=255)
+
+
+# Data sending controller
+class SerialPortController(Sofa.Core.Controller):
+
+    def __init__(self, *args, **kwargs):
+        Sofa.Core.Controller.__init__(self, *args, **kwargs)
+        self.name = "SerialPortController"
+        self.actuatedarms = kwargs['inputs']
+        self.serialport = kwargs['serialport']
+        self.serialport.packetOut = [150, 150, 150]
+        self.state = "init"
+
+    def onEndAnimationEvent(self):
+        # Data sending if the robot is initializing or in the no-communication sate
+        if self.state == "init":
+            return
+
+        if self.state == "no-comm":
+            return
+
+        # Vector storing the simulated servomotors' angular position
+        angles = []
+
+        for arm in self.actuatedarms:
+            # Conversion of the angle values from radians to degrees
+            angleDegree = arm.ServoMotor.angleOut*360/(2.0*math.pi)
+            angleByte = int(math.floor(angleDegree)) + 179
+
+            # Limitation of the angular position's command
+            if angleByte < 60:
+                angleByte = 60
+            if angleByte > 180:
+                angleByte = 180
+
+            # Filling the list of the 3 angle values
+            angles.append(angleByte)
+
+        # The controller board of the real robot receives `angles` values
+        self.serialport.packetOut = angles
+
+
+class InverseController(Sofa.Core.Controller):
+    """This controller has two role:
+       - if user press up/left/right/down/plus/minus the servo motor angle
+         is changed.
+       - if user press A an animation is started to move the motor to the physical position
+         they are occupying in the real robot.
+    """
+
+    def __init__(self, *args, serialport=None, servomotors=None, **kwargs):
+        Sofa.Core.Controller.__init__(self, *args, **kwargs)
+        self.name = "InverseController"
+        self.nodeGoal = args[1]
+        self.nodesInverseComponents = args[2]
+        self.nodeDofRigid = args[3]
+        self.nodeTripod = args[4]
+        self.serialport = args[5]
+        self.serialport.packetOut = [150, 150, 150]
+        self.state = "init"
+        self.actuators = servomotors
+        self.activate = False
+
+    def onKeypressedEvent(self, event):
+        key = event['key']
+        if key == Key.I:
+            for i in range(3):
+                self.nodeTripod.actuatedarms[i].ServoMotor.Articulation.RestShapeSpringsForceField.stiffness.value = [0.]
+            self.activate = True
+            for node in self.nodesInverseComponents:
+                node.activated = bool(self.activate)
+                node.init()
+
+    def onAnimateBeginEvent(self,e):
+
+        if self.state == "init":
+            return
+
+        if self.state == "no-comm":
+            return
+
+        if(self.activate):
+            #W_R_Dof = [0]*4;
+            #W_R_Ref = [0]*4;
+            #Ndir = [[ ]]*3;
+            Angles = [0]*3;
+
+            Angles[0] = self.nodeTripod.actuatedarms[0].ServoMotor.Articulation.dofs.position[0][0];
+            Angles[1] = self.nodeTripod.actuatedarms[1].ServoMotor.Articulation.dofs.position[0][0];
+            Angles[2] = self.nodeTripod.actuatedarms[2].ServoMotor.Articulation.dofs.position[0][0];            
+            AnglesOut = [];
+            for i in range(3):
+                # Conversion of the angle values from radians to degrees
+                angleDegree = Angles[i]*360/(2.0*math.pi)
+                angleByte = int(math.floor(angleDegree)) + 179
+
+                # Limitation of the angular position's command
+                if angleByte < 60:
+                    angleByte = 60
+                if angleByte > 180:
+                    angleByte = 180
+
+                # Filling the list of the 3 angle values
+                AnglesOut.append(angleByte)
+                
+            # The controller board of the real robot receives `AnglesOut` values
+            if(self.serialport):
+                self.serialport.packetOut = [AnglesOut[0], AnglesOut[1],  AnglesOut[2]]
+                #self.serialport.packetOut = [int(AnglesDeg[0]+0), int(AnglesDeg[1]-0), int(AnglesDeg[2])]
+
+
+class DirectController(Sofa.Core.Controller):
+    """This controller has two role:
+       - if user press up/left/right/down/plus/minus the servo motor angle
+         is changed.
+       - if user press A an animation is started to move the motor to the physical position
+         they are occupying in the real robot.
+    """
+
+    def __init__(self, *args, **kwargs):
+        Sofa.Core.Controller.__init__(self, *args, **kwargs)
+        self.name = "TripodController"
+        self.stepsize = 0.1
+        self.actuators = args[1]
+        self.serialportctrl = args[2]
+
+    def onKeypressedEvent(self, event):
+        key = event['key']
+        if key == Key.A :#and self.serialportctrl.state == "init":
+            self.serialportctrl.state = "no-comm"
+            animate(setupanimation, {"actuators": self.actuators, "step": 35.0, "angularstep": -1.4965}, duration=0.2)
+
+        # Inclusion of the keystroke to start data sending = establishing communication ('comm')
+        if key == Key.B and self.serialportctrl.state == "no-comm":
+            self.serialportctrl.state = "comm"
+
+
+def createScene(rootNode):
+
+    scene = Scene(rootNode, iterative=False)
+    scene.addMainHeader()
+    scene.addObject('DefaultAnimationLoop')
+    scene.addObject('DefaultVisualManagerLoop')
+    scene.VisualStyle.displayFlags = "showBehavior"
+
+    tripod = Tripod(scene.Modelling)
+
+    scene.addObject(TripodController(scene, actuators=[tripod.ActuatedArm0,
+                                                        tripod.ActuatedArm1,
+                                                        tripod.ActuatedArm2]))
+
+    scene.Simulation.addChild(tripod.RigidifiedStructure)
+    scene.Simulation.addObject('MechanicalMatrixMapper',
+                                 template='Vec3,Rigid3',
+                                 object1="@RigidifiedStructure/DeformableParts/dofs",
+                                 object2="@RigidifiedStructure/RigidParts/dofs",
+                                 nodeToParse="@RigidifiedStructure/DeformableParts/ElasticMaterialObject")
+    motors = scene.Simulation.addChild("Motors")
+    motors.addChild(tripod.ActuatedArm0)
+    motors.addChild(tripod.ActuatedArm1)
+    motors.addChild(tripod.ActuatedArm2)
