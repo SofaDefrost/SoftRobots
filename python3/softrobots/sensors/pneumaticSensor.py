08a5d2810502a7fadd40f8c6fb10ff8b993e5339
Alex Bilger <alxbilger@users.noreply.github.com>
2023-02-06 13:18:45 +0100

    Update stlib to stlib3 (#199)

--- python3/softrobots/sensors/pneumaticSensor.py
+++ python3/softrobots/sensors/pneumaticSensor.py
@@ -61,7 +61,7 @@ def PneumaticSensor(surfaceMeshFileName=None,
                                            attachedTo=attachedTo,
                                            name=name)
 
-    # This add a MeshSTLLoader, a componant loading the topology of the cavity.
+    # This add a MeshSTLLoader, a component loading the topology of the cavity.
     if surfaceMeshFileName.endswith(".stl"):
         PneumaticSensor.addObject('MeshSTLLoader', name='MeshLoader', filename=surfaceMeshFileName, rotation=rotation, translation=translation, scale=uniformScale)
     elif surfaceMeshFileName.endswith(".obj"):
@@ -70,11 +70,11 @@ def PneumaticSensor(surfaceMeshFileName=None,
         Sofa.msg_error("Your surfaceMeshFileName extension is not the right one, you have to give a surfacic mesh with .stl or .obj extension")
         return None
 
-    # This add a MeshTopology, a componant holding the topology of the cavity.
+    # This add a MeshTopology, a component holding the topology of the cavity.
     # PneumaticSensor.addObject('MeshTopology', name="topology", filename=surfaceMeshFileName)
     PneumaticSensor.addObject('MeshTopology', name='topology', src='@MeshLoader')
 
-    # This add a MechanicalObject, a componant holding the degree of freedom of our
+    # This add a MechanicalObject, a component holding the degree of freedom of our
     # mechanical modelling. In the case of a cavity actuated with PneumaticSensor, it is a set of positions specifying
     # the points where the pressure is applied.
     PneumaticSensor.addObject('MechanicalObject', src="@topology")
