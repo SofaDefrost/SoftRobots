7c142c6837788a4cf81f34a7ac5415489aad0062
eulalie <eulalie.coevoet@gmail.com>
2023-04-25 11:29:58 +0200

    [src] cleaning files header

--- src/SoftRobots/component/engine/VolumeFromTriangles.cpp
+++ src/SoftRobots/component/engine/VolumeFromTriangles.cpp
@@ -1,23 +1,22 @@
 /******************************************************************************
-*       SOFA, Simulation Open-Framework Architecture                          *
-*                (c) 2006-2018 INRIA, USTL, UJF, CNRS, MGH                    *
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
 *                                                                             *
-* This library is free software; you can redistribute it and/or modify it     *
+* This program is free software; you can redistribute it and/or modify it     *
 * under the terms of the GNU Lesser General Public License as published by    *
 * the Free Software Foundation; either version 2.1 of the License, or (at     *
 * your option) any later version.                                             *
 *                                                                             *
-* This library is distributed in the hope that it will be useful, but WITHOUT *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
 * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
 * for more details.                                                           *
 *                                                                             *
 * You should have received a copy of the GNU Lesser General Public License    *
-* along with this library; if not, write to the Free Software Foundation,     *
-* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.          *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
 *******************************************************************************
-*                           Plugin SoftRobots    v1.0                         *
-*				                                              *
+*                           Plugin SoftRobots                                 *
+*                                                                             *
 * This plugin is also distributed under the GNU LGPL (Lesser General          *
 * Public License) license with the same conditions than SOFA.                 *
 *                                                                             *
@@ -25,7 +24,6 @@
 *               Ecole Centrale de Lille)                                      *
 *                                                                             *
 * Contact information: https://project.inria.fr/softrobot/contact/            *
-*                                                                             *
 ******************************************************************************/
 #include <SoftRobots/component/engine/VolumeFromTriangles.inl>
 #include <sofa/core/ObjectFactory.h>
