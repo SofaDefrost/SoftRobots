3adae942b3acafd7b4bf0961500c902f6fcdc208
Alex Bilger <alxbilger@users.noreply.github.com>
2023-08-28 17:38:40 +0200

    Speedup SurfacePressureConstraint.py (#237)
    Before:
    100 iterations done in 7.06994 s ( 14.1444 FPS)
    After:
    100 iterations done in 4.05716 s ( 24.6478 FPS)

--- examples/component/constraint/SurfacePressureConstraint/SurfacePressureConstraint.py
+++ examples/component/constraint/SurfacePressureConstraint/SurfacePressureConstraint.py
@@ -5,30 +5,30 @@ path = os.path.dirname(os.path.abspath(__file__)) + '/mesh/'
 
 
 def createScene(rootNode):
-    rootNode.addObject('RequiredPlugin', name='SoftRobots')
-    rootNode.addObject('RequiredPlugin', name='SofaPython3')
-    rootNode.addObject('RequiredPlugin', pluginName=[
-                                "Sofa.Component.AnimationLoop",  # Needed to use components FreeMotionAnimationLoop
-                                "Sofa.Component.Constraint.Lagrangian.Correction",
-                                # Needed to use components LinearSolverConstraintCorrection
-                                "Sofa.Component.Constraint.Lagrangian.Solver",  # Needed to use components GenericConstraintSolver
-                                "Sofa.Component.Engine.Select",  # Needed to use components BoxROI
-                                "Sofa.Component.IO.Mesh",  # Needed to use components MeshOBJLoader, MeshVTKLoader
-                                "Sofa.Component.LinearSolver.Direct",  # Needed to use components SparseLDLSolver
-                                "Sofa.Component.LinearSolver.Iterative",  # Needed to use components ShewchukPCGLinearSolver
-                                "Sofa.Component.Mass",  # Needed to use components UniformMass
-                                "Sofa.Component.ODESolver.Backward",  # Needed to use components EulerImplicitSolver
-                                "Sofa.Component.SolidMechanics.FEM.Elastic",  # Needed to use components TetrahedronFEMForceField
-                                "Sofa.Component.SolidMechanics.Spring",  # Needed to use components RestShapeSpringsForceField
-                                "Sofa.Component.Topology.Container.Constant",  # Needed to use components MeshTopology
-                                "Sofa.Component.Topology.Container.Dynamic",
-                                # Needed to use components TetrahedronSetTopologyContainer, TetrahedronSetTopologyModifier, TriangleSetTopologyContainer, TriangleSetTopologyModifier
-                                "Sofa.Component.Topology.Mapping",  # Needed to use components Tetra2TriangleTopologicalMapping
-                                "Sofa.Component.Visual",  # Needed to use components VisualStyle
-                                "Sofa.GL.Component.Rendering3D",  # Needed to use components OglModel
-                            ])
+
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.AnimationLoop') # Needed to use components [FreeMotionAnimationLoop]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Lagrangian.Correction') # Needed to use components [LinearSolverConstraintCorrection]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Lagrangian.Solver') # Needed to use components [GenericConstraintSolver]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Engine.Select') # Needed to use components [BoxROI]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.IO.Mesh') # Needed to use components [MeshOBJLoader,MeshVTKLoader]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.LinearSolver.Direct') # Needed to use components [EigenSimplicialLDLT]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mapping.Linear') # Needed to use components [BarycentricMapping,IdentityMapping]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mass') # Needed to use components [UniformMass]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.ODESolver.Backward') # Needed to use components [EulerImplicitSolver]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.SolidMechanics.FEM.Elastic') # Needed to use components [FastTetrahedralCorotationalForceField]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.SolidMechanics.Spring') # Needed to use components [RestShapeSpringsForceField]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.StateContainer') # Needed to use components [MechanicalObject]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Container.Constant') # Needed to use components [MeshTopology]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Container.Dynamic') # Needed to use components [TetrahedronSetTopologyContainer,TetrahedronSetTopologyModifier,TriangleSetTopologyContainer,TriangleSetTopologyModifier]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Mapping') # Needed to use components [Tetra2TriangleTopologicalMapping]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Visual') # Needed to use components [VisualStyle]
+    rootNode.addObject('RequiredPlugin', name='Sofa.GL.Component.Rendering3D') # Needed to use components [OglModel]
+    rootNode.addObject('RequiredPlugin', name='SoftRobots') # Needed to use components [SurfacePressureConstraint]
+
     rootNode.addObject('VisualStyle',
-                       displayFlags='showVisualModels hideBehaviorModels showCollisionModels hideBoundingCollisionModels hideForceFields showInteractionForceFields hideWireframe')
+                       displayFlags='showVisualModels hideBehaviorModels showCollisionModels '
+                                    'hideBoundingCollisionModels hideForceFields showInteractionForceFields '
+                                    'hideWireframe')
 
     rootNode.addObject('FreeMotionAnimationLoop')
     rootNode.addObject('DefaultVisualManagerLoop')
@@ -37,17 +37,17 @@ def createScene(rootNode):
     # bunny
     bunny = rootNode.addChild('bunny')
     bunny.addObject('EulerImplicitSolver', name='odesolver')
-    bunny.addObject('SparseLDLSolver')
+    bunny.addObject('EigenSimplicialLDLT', template='CompressedRowSparseMatrixMat3x3')
     bunny.addObject('MeshVTKLoader', name='loader', filename=path + 'Hollow_Stanford_Bunny.vtu')
     bunny.addObject('TetrahedronSetTopologyContainer', src='@loader', name='container')
     bunny.addObject('TetrahedronSetTopologyModifier')
 
     bunny.addObject('MechanicalObject', name='tetras', template='Vec3', showIndices=False)
     bunny.addObject('UniformMass', totalMass=0.5)
-    bunny.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.3,
+    bunny.addObject('FastTetrahedralCorotationalForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.3,
                     youngModulus=18000)
 
-    bunny.addObject('BoxROI', name='boxROI', box=[-5, -15, -5, 5, -4.5, 5], drawBoxes=True,
+    bunny.addObject('BoxROI', name='boxROI', box=[-5, -7, -5, 5, -4.5, 5], drawBoxes=True,
                     position="@tetras.rest_position", tetrahedra="@container.tetrahedra")
     bunny.addObject('RestShapeSpringsForceField', points='@boxROI.indices', stiffness=1e12)
     bunny.addObject('LinearSolverConstraintCorrection')
