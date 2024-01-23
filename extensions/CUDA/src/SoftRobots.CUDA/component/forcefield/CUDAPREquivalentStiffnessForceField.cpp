ab277b747a9e619accca5da74ecfba6368afc07d
Alex Bilger <alexbilger0@gmail.com>
2024-01-23 11:18:07 +0100

    CUDA template specialization

---
+++ extensions/CUDA/src/SoftRobots.CUDA/component/forcefield/CUDAPREquivalentStiffnessForceField.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+*                 SOFA, Simulation Open-Framework Architecture                *
+*                    (c) 2006 INRIA, USTL, UJF, CNRS, MGH                     *
+*                                                                             *
+* This program is free software; you can redistribute it and/or modify it     *
+* under the terms of the GNU Lesser General Public License as published by    *
+* the Free Software Foundation; either version 2.1 of the License, or (at     *
+* your option) any later version.                                             *
+*                                                                             *
+* This program is distributed in the hope that it will be useful, but WITHOUT *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License *
+* for more details.                                                           *
+*                                                                             *
+* You should have received a copy of the GNU Lesser General Public License    *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+*******************************************************************************
+*                           Plugin SoftRobots                                 *
+*                                                                             *
+* This plugin is also distributed under the GNU LGPL (Lesser General          *
+* Public License) license with the same conditions than SOFA.                 *
+*                                                                             *
+* Contributors: Defrost team  (INRIA, University of Lille, CNRS,              *
+*               Ecole Centrale de Lille)                                      *
+*                                                                             *
+* Contact information: https://project.inria.fr/softrobot/contact/            *
+******************************************************************************/
+#include <SoftRobots.CUDA/config.h>
+#include <SofaCUDA/sofa/gpu/cuda/CudaTypes.h>
+#include <SoftRobots/component/forcefield/PREquivalentStiffnessForceField.inl>
+#include <sofa/core/ObjectFactory.h>
+
+namespace softrobots::forcefield
+{
+template class SOFA_SOFTROBOTS_CUDA_API PREquivalentStiffnessForceField<sofa::gpu::cuda::CudaRigid3fTypes>;
+
+#ifdef SOFA_GPU_CUDA_DOUBLE
+template class SOFA_SOFTROBOTS_CUDA_API PREquivalentStiffnessForceField<sofa::gpu::cuda::CudaRigid3dTypes>;
+#endif
+}
+
+int CUDAPREquivalentStiffnessForceFieldClass = sofa::core::RegisterObject("Supports GPU-side computations using CUDA")
+    .add< softrobots::forcefield::PREquivalentStiffnessForceField<sofa::gpu::cuda::CudaRigid3fTypes> >()
+#ifdef SOFA_GPU_CUDA_DOUBLE
+    .add< softrobots::forcefield::PREquivalentStiffnessForceField<sofa::gpu::cuda::CudaRigid3dTypes> >()
+#endif
+;
+
